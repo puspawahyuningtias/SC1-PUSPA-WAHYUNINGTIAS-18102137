ebaf29726ce45d7b37b93cd63d37a5b212048e8c
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-06-10 10:43:29 +0700

    PHP Variables

---
+++ Practice8/PHP_Variables.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+    <!-- Creating (Declaring) PHP Variables -->
+    <?php
+    $txt = "Hello world!";
+    $x = 5;
+    $y = 10.5;
+    ?>
+    <br>
+    <!-- Output Variables -->
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love $txt!";
+    ?>
+    <br>
+    <?php
+    $txt = "W3Schools.com";
+    echo "I love " . $txt . "!";
+    ?>
+    <br>
+    <?php
+    $x = 5;
+    $y = 4;
+    echo $x + $y;
+    ?>
+</body>
+
+</html>
