2500e67868b91230fa7b3b67eeff687aaa4b1d85
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-06-14 18:46:48 +0700

    PHP Operators

---
+++ Practice8/PHP_Operators.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+
+<body>
+
+    <?php
+
+    $x = 10;
+    $y = 6;
+
+    echo $x + $y;
+    echo "<hr>";
+    echo $x - $y;
+    echo "<hr>";
+    echo $x * $y;
+    echo "<hr>";
+    echo $x / $y;
+    echo "<hr>";
+    echo $x % $y;
+    echo "<hr>";
+    echo $x ** $y;
+    echo "<hr>";
+    $x = 10;
+    echo $x;
+    echo "<hr>";
+    $x = 20;
+    $x += 100;
+    echo $x;
+    echo "<hr>";
+    $x = 50;
+    $x -= 30;
+    echo $x;
+    echo "<hr>";
+    $x = 5;
+    $x *= 6;
+    echo $x;
+    echo "<hr>";
+    $x = 10;
+    $x /= 5;
+    echo $x;
+    echo "<hr>";
+    $x = 15;
+    $x %= 4;
+    echo $x;
+    echo "<hr>";
+
+    ?>
+
+</body>
+
+</html>
