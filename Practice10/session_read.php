764a2a84ace2af58c867513fe2ae20fd5bc134ba
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-06-23 18:13:31 +0700

    menambahkan session start

--- Practice10/session_read.php
+++ Practice10/session_read.php
@@ -11,6 +11,9 @@
         <input type="text" name="kolom_input_session2" required /></br>
         <button type="submit">Simpan</button>
     </form>
+    <?php session_start() ?>
+    Session yang tersimpan 1: <?php echo isset($_SESSION['session_tersimpan1']) ? $_SESSION['session_tersimpan1'] : 'Session 1 Kosong' ?><br>
+    Session yang tersimpan 2: <?php echo isset($_SESSION['session_tersimpan2']) ? $_SESSION['session_tersimpan2'] : 'Session 2 Kosong' ?>
 
 </body>
 
