e060ccbd1ffe430705b99040813db4687e044097
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-06-23 18:09:22 +0700

    Menambahkan form session process

--- Practice10/session_read.php
+++ Practice10/session_read.php
@@ -6,6 +6,12 @@
 </head>
 
 <body>
+    <form action="session_process.php" method="post">
+        <input type="text" name="kolom_input_session1" required /></br>
+        <input type="text" name="kolom_input_session2" required /></br>
+        <button type="submit">Simpan</button>
+    </form>
+
 </body>
 
 </html>
