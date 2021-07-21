0474f6960d9338e0678a61fb6cf42840e6d40400
PUSPA WAHYUNINGTIAS <18102137@ittelkom-pwt.ac.id>
2021-07-21 15:28:22 +0700

    Create Students

---
+++ practice_laravel/laravel/app/Http/Controllers/StudentApiController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Student;
+use Illuminate\Http\Request;
+use Validator;
+use File;
+
+
+class StudentApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $validateData = Validator::make($request->all(), [
+            'nim' => 'required|size:8,unique:student,nim',
+            'nama' => 'required|min:3|max:50',
+            'jenis_kelamin' => 'required|in:P,L',
+            'jurusan' => 'required',
+            'alamat' => '',
+            'image' => 'required|file|image|max:1000',
+        ]);
+        if ($validateData->fails()) {
+            return response($validateData->errors(), 400);
+        } else {
+            $mahasiswa = new Student();
+            $mahasiswa->nim = $request->nim;
+            $mahasiswa->name = $request->nama;
+            $mahasiswa->gender = $request->jenis_kelamin;
+            $mahasiswa->departement = $request->jurusan;
+            $mahasiswa->address = $request->alamat;
+            if ($request->hasFile('image')) {
+                $extFile = $request->image->getClientOriginalExtension();
+                $namaFile = 'user-' . time() . "." . $extFile;
+                $path = $request->image->move('assets/images', $namaFile);
+                $mahasiswa->image = $path;
+            }
+            $mahasiswa->save();
+            return response()->json([
+                "message" => "student record created"
+            ], 201);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
